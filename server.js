8d6ccf8785d5d6d87b12b95502ce0f4ed9895e5e
wmahathre <wmahathre@gmail.com>
2019-02-07 21:17:19 -0500

    Changes express.static from client to game to reflect file system

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -9,7 +10,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("game/build"));
 }
 
 // Add routes, both API and view
@@ -17,6 +18,10 @@ require('./routes/api-routes')(app);
 //srequire('./routes/routes')(app);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clickyGame");
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/game/build/index.html'));
+});
 
 // Start the API server
 app.listen(PORT, function() {
